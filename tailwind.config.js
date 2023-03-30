fe09bf5f2852e7ab21b3c0430275555f2527c0fd
Badie Alili <yacineabdelbadiealili@gmail.com>
2023-03-31 00:51:51 +0100

    Migrating from cra to vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
-  content: [],
-=======
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html"
   ],
->>>>>>> 0ed99eb1b3c3dbbd927636884d4609bc8391a87e
   theme: {
     extend: {},
   },
