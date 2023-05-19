20dfb065ae08f5b1c512c02184f50b87c82f5bec
1mt4y <t.madani@esi-sba.dz>
2023-05-19 18:29:15 +0100

    :zap: finally fixed meeting room :zap:

--- src/pages/MeetingRoom.jsx
+++ src/pages/MeetingRoom.jsx
@@ -1,18 +1,18 @@
 import React, { useState, useEffect, useRef } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate, Navigate } from "react-router-dom";
 import { useAuthContext } from "../hooks/useAuthContext";
 
 import { io } from "socket.io-client";
 import Peer from "peerjs";
 
-const socket = io("http://localhost:10000");
 
 const MeetingRoom = () => {
-  const navigate = useNavigate();
+  const { user } = useAuthContext();
+  const [loading, setLoading] = useState(true);
 
   const { roomId } = useParams();
-  const { user } = useAuthContext();
   const [roomExists, setRoomExists] = useState(false);
+  const videoGrid = useRef();
 
   useEffect(() => {
     async function checkRoomExists() {
@@ -25,82 +25,103 @@ const MeetingRoom = () => {
       );
 
       const json = await response.json();
-      console.log(json.message);
-
-      if (response.ok) {
-        // TODO: check json response attribute name with backend
+      if (json && json.message == "Room exists") {
         setRoomExists(true);
       }
+      setLoading(false);
     }
     checkRoomExists();
   }, []);
 
-  if (roomExists) {
-    // TODO: this doesn't work idk why, it's 1am so i'll fix it tomorrow
-    return <div>Room Doesn't Exist</div>;
+  function onConnect() {
+    // TODO: for error handling, whenever our socket disconnects
+    // we can for example reload the page to force a reconnect or redirect to /error ...etc
+    console.log("socket connected");
   }
 
-  const videoGrid = useRef();
-  const myPeer = new Peer();
-  const myVideo = document.createElement("video");
-  myVideo.muted = true;
-  const peers = {};
+  function onDisconnect() {
+    // TODO: for error handling, whenever our socket disconnects
+    // we can for example reload the page to force a reconnect or redirect to /error ...etc
+    console.log("socket disconnected");
+  }
 
-  navigator.mediaDevices
-    .getUserMedia({ video: true, audio: true })
-    .then((stream) => {
-      // add my video stream to video grid
-      addVideoStream(myVideo, stream);
+  useEffect(() => {
+    if (roomExists) {
+      const socket = io("http://localhost:10000", {
+        autoConnect: false
+      });
+      const myPeer = new Peer({
+        // host: '/',
+        // port: '3001',
+      });
+      socket.on('connect', onConnect);
+      socket.on('disconnect', onDisconnect);
+      
+      const myVideo = document.createElement("video");
+      myVideo.muted = true;
+      const peers = {};
+      myPeer.on("open", (id) => {
+        socket.emit("join-room", roomId, id);
+      });
 
-      myPeer.on("call", (call) => {
-        console.log("call received");
-        call.answer(stream);
+      navigator.mediaDevices
+        .getUserMedia({ video: true, audio: true })
+        .then((stream) => {
+          // add my video stream to video grid
+          addVideoStream(myVideo, stream);
+  
+          myPeer.on("call", (call) => {
+            call.answer(stream);
+            const video = document.createElement("video");
+            call.on("stream", (userVideoStream) => {
+              addVideoStream(video, userVideoStream);
+            });
+          });
+
+          socket.on("user-connected", (userId) => {
+            connectToNewUser(userId, stream);
+          });
+  
+          socket.on("user-disconnected", (userId) => {
+            if (peers[userId]) peers[userId].close();
+          });
+        });
+  
+      function connectToNewUser(userId, stream) {
+        const call = myPeer.call(userId, stream);
         const video = document.createElement("video");
         call.on("stream", (userVideoStream) => {
           addVideoStream(video, userVideoStream);
         });
-      });
-
-      socket.on("user-connected", (userId) => {
-        console.log("user-connected received");
-        connectToNewUser(userId, stream);
-      });
-
-      socket.on("user-disconnected", (userId) => {
-        if (peers[userId]) peers[userId].close();
-      });
-
-      myPeer.on("open", (id) => {
-        socket.emit("join-room", roomId, id);
-      });
-    });
-
-  function connectToNewUser(userId, stream) {
-    const call = myPeer.call(userId, stream);
-    const video = document.createElement("video");
-    call.on("stream", (userVideoStream) => {
-      addVideoStream(video, userVideoStream);
-    });
-    call.on("close", () => {
-      video.remove();
-    });
-
-    peers[userId] = call;
-  }
-
-  function addVideoStream(video, stream) {
-    if (video.current) {
-      video.current.srcObject = stream;
-    } else {
-      video.srcObject = stream;
+        call.on("close", () => {
+          video.remove();
+        });
+  
+        peers[userId] = call;
+      }
+  
+      function addVideoStream(video, stream) {
+        if (video.current) {
+          video.current.srcObject = stream;
+        } else {
+          video.srcObject = stream;
+        }
+        video.addEventListener("loadedmetadata", () => {
+          video.play();
+        });
+        videoGrid.current.append(video);
+      }
     }
-    video.addEventListener("loadedmetadata", () => {
-      video.play();
-    });
-    videoGrid.current.append(video);
-  }
-
-  return <div id="video-grid" ref={videoGrid}></div>;
+  }, [roomExists])
+  
+  return loading ? (
+    // TODO: style loading better? (optional)
+    <div id="video-grid" ref={videoGrid}>Loading</div>
+  ) : roomExists ? (
+    <div id="video-grid" ref={videoGrid}></div>
+  ) : (
+    <Navigate to="/error" />
+  );
 };
 
 export default MeetingRoom;
