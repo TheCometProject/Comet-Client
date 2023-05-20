909dfbb77f71f63bd3b9cc5513887a7ead695e4c
bouchrahr <bouchra.harrath5@gmail.com>
2023-05-20 03:27:34 +0200

    tos page +some modifications in landing page

--- src/components/Landing/communityData.js
+++ src/components/Landing/communityData.js
@@ -4,27 +4,27 @@ export default [
     name: "Sarah simpson",
     title: "Twitter CEO",
     statement:
-      "Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit.",
+    "Comet's feature set is impressive. From screen sharing and note-taking to interactive polls and collaborative whiteboard, it has everything I need to enhance my virtual collaboration experience. It has truly exceeded my expectations.",
   },
   {
     image: "src/pages/Assets/Avatars/avatar03.png",
     name: "Fred James",
     title: "Security engineer",
     statement:
-      "Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit.",
+    "Comet has revolutionized my virtual meetings! The screen sharing feature is seamless and allows me to collaborate effectively with my team. Highly recommended!",
   },
   {
     image: "src/pages/Assets/Avatars/avatar04.png",
     name: "Lily Geller",
     title: "Freelancer",
     statement:
-      "Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit.",
+    "Comet is a game-changer in the video conferencing industry. The level of security and privacy measures implemented by the platform is commendable, ensuring a worry-free and confidential communication environment.",
   },
   {
     image: "src/pages/Assets/Avatars/avatar06.png",
     name: "Joe Mama",
     title: "Student",
     statement:
-      "Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit.",
+    "I love using Comet for my virtual meetings. It offers a seamless and intuitive interface, making it effortless to connect with colleagues and clients. It has definitely improved my productivity.",
   },
 ];
