598d5980faa1e7723d3a665fe2121915a7ef14c4
Badie Alili <yacineabdelbadiealili@gmail.com>
2023-06-03 12:48:51 +0100

    added left meeting setup

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,17 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import heroimg from "../Assets/Images/image 3.png";
 import img1 from "../Assets/Logo.png";
 import prfl from "../Assets/profilsvg.svg";
 import camera from "../Assets/Icons/Button camera.svg";
 import keyboard from "../Assets/Icons/Keyboard.svg";
+import { API_URL } from "../constants";
 
 const Dashboard = () => {
-  const [roomID, setRoomId] = useState("");
+  const [roomId, setRoomId] = useState("");
+  const navigate = useNavigate();
 
   function handleChange(e) {
     setRoomId(() => e.target.value);
+  }
+
+  async function handleJoinMeeting() {
+    const res = await fetch(`${API_URL}/api/v1/rooms/${roomId}`);
+    const json = await res.json();
+    if(json.message == "Room exists"){
+      navigate("/setup");
+    } else {
+      navigate("/error")
+    }
   }
 
   return (
@@ -68,10 +80,17 @@ const Dashboard = () => {
                   type="text"
                   className="button-outlined !pl-12  !pr-0 placeholder:text-blue-700"
                   placeholder="Enter the space code"
-                  value={roomID}
+                  value={roomId}
                 />
               </div>
-              <button className="button-invisible">Join</button>
+              {/* <Link to="setup"> */}
+                <button
+                  onClick={handleJoinMeeting}
+                  className="button-invisible"
+                >
+                  Join
+                </button>
+              {/* </Link> */}
             </div>
           </div>
         </div>
